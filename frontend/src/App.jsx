7fca8d6407510e77ad18f7cebe414af1ab298eb7
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-06-18 14:24:07 +0530

    Navber added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import MainNavigation from "./components/MainNavigation";
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import Home from "./Pages/Home"
+import Login from "./Pages/Login";
+import MyTasks from "./Pages/MyTasks";
+import AddTask from "./Pages/Addtask";
+import EditTask from "./Pages/EditTask";
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<MainNavigation />,
+    children:[
+      {
+        path:"/",
+        element:<Home />
+      },
+      {
+        path:"/myTasks",
+        element:<MyTasks />
+      },
+      {
+        path:"/addTask",
+        element:<AddTask />
+      },
+      {
+        path:"/editTask",
+        element:<EditTask />
+      },
+      {
+        path:"/login",
+        element:<Login />
+      }
+    ]
+  }
+])
 
 const App = () => {
   return (
-    <div className='text-4xl text-red-500'>App</div>
-  )
-}
+   <RouterProvider router={router}></RouterProvider>
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="w-full flex justify-between px-6 py-3 container  shadow-md">
+        <Link to="/">
+          <h1 className="text-4xl font-bold">Todo</h1>
+        </Link>
+        <ul className="flex space-x-8 text-xl">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/MyTasks">
+            <li>My Task</li>
+          </Link>
+          <Link to="/AddTask">
+            <li>Add Task</li>
+          </Link>
+          <Link to="/login">
+            <li>Login</li>
+          </Link>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
