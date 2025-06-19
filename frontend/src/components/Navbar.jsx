dc4b9adbb2dfcc3a19e5ea6c70b22a9bc23fac6e
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-06-19 10:50:08 +0530

    updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,20 +6,30 @@ const Navbar = () => {
     <>
       <div className="w-full flex justify-between px-6 py-3  shadow-md">
         <Link to="/">
-          <h1 className="text-4xl font-bold hover:scale-105 duration-300 hover:text-blue-500">Todo</h1>
+          <h1 className="text-4xl font-bold hover:scale-105 duration-300 hover:text-blue-500">
+            Task Master
+          </h1>
         </Link>
         <ul className="flex space-x-8 text-xl">
           <Link to="/">
-            <li className="hover:scale-105 duration-300 hover:text-blue-500">Home</li>
+            <li className="hover:scale-105 duration-300 hover:text-blue-500">
+              Home
+            </li>
           </Link>
           <Link to="/MyTasks">
-            <li className="hover:scale-105 duration-300 hover:text-blue-500">My Task</li>
+            <li className="hover:scale-105 duration-300 hover:text-blue-500">
+              My Task
+            </li>
           </Link>
           <Link to="/AddTask">
-            <li className="hover:scale-105 duration-300 hover:text-blue-500">Add Task</li>
+            <li className="hover:scale-105 duration-300 hover:text-blue-500">
+              Add Task
+            </li>
           </Link>
           <Link to="/login">
-            <li className="hover:scale-105 duration-300 hover:text-blue-500">Login</li>
+            <li className="hover:scale-105 duration-300 hover:text-blue-500">
+              Login
+            </li>
           </Link>
         </ul>
       </div>
