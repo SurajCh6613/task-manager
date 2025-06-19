957c7a4322807485756899787c242ffe0fda5e24
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-06-19 08:26:57 +0530

    Cors setup

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,25 @@
-require("dotenv").config()
-const express = require('express')
-const app= express();
-const cookieParser = require('cookie-parser')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-const PORT = process.env.PORT || 3000
-const userRoutes = require('./routes/userRoutes')
-const taskRoutes = require('./routes/taskRoutes')
-const ConnectDB = require('./config/ConnectDB')
+const PORT = process.env.PORT || 3000;
+const userRoutes = require("./routes/userRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+const ConnectDB = require("./config/ConnectDB");
 
-ConnectDB() // DB Connection
-app.use(express.json())
-app.use(cookieParser())
-
+ConnectDB(); // DB Connection
+app.use(cors({origin:'http://localhost:5173',
+    credentials:true   // 👈 This allows cookies to be sent
+}));
+app.use(express.json());
+app.use(cookieParser());
 
 // All Routes
-app.use("/user",userRoutes)
-app.use("/task",taskRoutes)
+app.use("/user", userRoutes);
+app.use("/task", taskRoutes);
 
-app.listen(PORT,()=>{
-    console.log(`Server Running at : ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server Running at : ${PORT}`);
+});
