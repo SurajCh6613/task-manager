bb3b8f67a5ca4e62b30d7743c01f098353321a9b
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-06-21 12:56:33 +0530

    fetch user tasks and show them

--- frontend/src/Pages/MyTasks.jsx
+++ frontend/src/Pages/MyTasks.jsx
@@ -1,64 +1,66 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MdDelete } from "react-icons/md";
 import { FaEdit } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useAuth } from "../context/authContext";
 
 const MyTasks = () => {
-  const [tasks, setTasks] = useState([
-    {
-      title: "Learn React",
-      description:
-        "Complete Hooks Complete Hooks Complete Hooks v Complete Hooks Complete Hooks",
-      time: "2025-06-21",
-    },
-    {
-      title: "Learn React",
-      description:
-        "Complete Hooks Complete Hooks Complete Hooks v Complete Hooks Complete Hooks",
-      time: "2025-06-21",
-    },
-    {
-      title: "Learn React",
-      description:
-        "Complete Hooks Complete Hooks Complete Hooks v Complete Hooks Complete Hooks",
-      time: "2025-06-21",
-    },
-    {
-      title: "Learn React",
-      description:
-        "Complete Hooks Complete Hooks Complete Hooks v Complete Hooks Complete Hooks",
-      time: "2025-06-21",
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
+  const { loading, setLoading } = useAuth();
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const fetchTask = async () => {
+      try {
+        const taskRes = await axios.get(`http://localhost:3000/task/allTasks`, {
+          withCredentials: true,
+        });
+        setTasks(taskRes.data);
+      } catch (err) {
+        console.error("Error fetching tasks:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchTask();
+  }, []);
+
   return (
-    <div>
-      <div className="w-full p-4">
-        <div className="p-8 shadow-md rounded-md">
-          {tasks.length === 0 ? (
-            <p className="text-3xl text-gray-500 text-center">No Tasks Found</p>
-          ) : (
-            <ul className="grid md:grid-cols-2 lg:grid-cols-3 gap-3">
-              {tasks.map((task, index) => (
-                <li key={index} className="p-4 rounded hover:scale-105 duration-300 shadow-md">
-                  <h3 className="text-lg font-semibold">{task.title}</h3>
-                  <p className="text-sm text-gray-600">{task.description}</p>
-                  <p className="text-sm text-blue-600">Due: {task.time}</p>
-                  <div className="flex space-x-4 w-2/3">
-                    <Link to="/task/editTask">
-                      <button className="py-3">
-                        Edit <FaEdit className="inline h-6 w-6 text-gray-500" />
-                      </button>
-                    </Link>
-                    <button>
-                      Delete{" "}
-                      <MdDelete className="inline w-6 h-6 hover:scale-110 duration-200 text-red-500" />
-                    </button>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
+    <div className="w-full p-4">
+      <div className="p-8 shadow-md rounded-md">
+        {loading ? (
+          <p className="text-center text-gray-500 text-xl">Loading...</p>
+        ) : tasks.length === 0 ? (
+          <p className="text-3xl text-gray-500 text-center">No Tasks Found</p>
+        ) : (
+          <ul className="grid md:grid-cols-2 lg:grid-cols-3 gap-3">
+            {tasks.map((task, index) => (
+              <li
+                key={task._id}
+                className="p-4 rounded hover:scale-105 duration-300 shadow-md"
+              >
+                <h3 className="text-lg font-semibold">{task.title}</h3>
+                <p className="text-sm text-gray-600">{task.description}</p>
+                <p className="text-sm text-blue-600">
+                  Due:{" "}
+                  {task.date
+                    ? new Date(task.date).toLocaleDateString()
+                    : "No Date"}
+                </p>
+                <div className="flex space-x-4 w-2/3">
+                  <Link to={`/task/editTask/${task._id}`}>
+                    Edit <FaEdit className="inline h-6 w-6 text-gray-500" />
+                  </Link>
+                  <button>
+                    Delete{" "}
+                    <MdDelete className="inline w-6 h-6 hover:scale-110 duration-200 text-red-500" />
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
