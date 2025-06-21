54287a3550d5bb352cf29ca039f0d0a6f62b34d2
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-06-21 11:19:37 +0530

    myTask route changed to allTasks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ const router = createBrowserRouter([
         element: <PrivateRoute />,
         children: [
           {
-            path: "/myTasks",
+            path: "/allTasks",
             element: <MyTasks />,
           },
           {
