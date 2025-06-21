4e78340089f5f7eed18204c48ce740f83619d4ec
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-06-21 08:41:30 +0530

    loading from authContext

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -2,13 +2,9 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/authContext";
 
 const PrivateRoute = () => {
-  const { user } = useAuth();
-  console.log(user);
-
-  if (user === null)
-    return <p className="flex text-4xl text-center">Loading...</p>;
-  if (user === false) return <Navigate to="/login" />;
-  return <Outlet />;
+  const { user, loading } = useAuth();
+  if (loading) return <div>Loading...</div>;
+  return user ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
