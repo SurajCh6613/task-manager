86c0630490f3b4cce7c9e24dcb6e6bb6190f699c
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-06-22 10:18:10 +0530

    update Profile implemented

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -1,17 +1,21 @@
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/authContext";
 import Logout from "../components/Logout";
+import { useEffect } from "react";
 
 const Dashboard = () => {
   const { user } = useAuth();
+
   return (
     <div className="w-full p-8">
       <div className="p-8 rounded-md shadow-md flex justify-between">
         <div>
           <h3 className="text-3xl font-semibold mb-4">Welcome, {user.name}</h3>
           <p className="mb-4 text-gray-500">Email: {user.email}</p>
-          <button className="btn mb-4">Edit Profile</button>
+          <Link className="btn mb-4" to={`/user/edit/${user._id}`}>
+            Update Profile
+          </Link>
         </div>
         <div>
           <Logout />
---
+++ frontend/src/Pages/UpdateProfile.jsx
@@ -0,0 +1,87 @@
+import axios from "axios";
+import { useAuth } from "../context/authContext";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const UpdateProfile = () => {
+  const navigate = useNavigate();
+  const { user, loading, setLoading, setUser } = useAuth();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+  });
+
+  //   useEffect to set user data in formData
+  useEffect(() => {
+    setFormData({
+      name: user.name || "",
+      email: user.email || "",
+    });
+  }, [user]);
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.put(
+        `http://localhost:3000/user/edit/${user._id}`,
+        formData,
+        {
+          withCredentials: true,
+        }
+      );
+
+      setUser(res.data.user);
+
+      navigate("/dashboard");
+    } catch (error) {
+      console.log("Update User Error", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) return <div>Loading...</div>;
+  if (loading || !user) return <div>Loading...</div>;
+
+  return (
+    <div className="w-full p-8">
+      <div className="p-8 rounded-md shadow-md flex justify-between">
+        <div>
+          <h3 className="text-3xl font-semibold mb-4">Update Profile</h3>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+            <div className="flex gap-3">
+              <label className="pt-3 w-20">Name</label>
+              <input
+                type="text"
+                name="name"
+                className="input"
+                value={formData.name}
+                onChange={handleOnChange}
+              />
+            </div>
+            <div className="flex gap-3">
+              <label className="pt-3 w-20">Email</label>
+              <input
+                type="email"
+                name="email"
+                className="input"
+                value={formData.email}
+                onChange={handleOnChange}
+              />
+            </div>
+            <button type="submit" className="btn mb-4">
+              {loading ? "Processing..." : "Update"}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UpdateProfile;
